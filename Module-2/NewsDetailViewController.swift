bc34cd62931982cabe4caf07e1bf6a54940d525e
Paul Defilippi <allinfo.pd@gmail.com>
2019-08-12 19:56:44 -0400

    made adjustments to News Detail VC

--- Module-2/NewsDetailViewController.swift
+++ Module-2/NewsDetailViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import WebKit
+import Alamofire
 
 class NewsDetailViewController: UIViewController, WKNavigationDelegate {
     
@@ -17,19 +18,53 @@ class NewsDetailViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet weak var webView: WKWebView?
     @IBOutlet weak var heightConstraint: NSLayoutConstraint?
     
-    var author: String?
+    var article: Article?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel?.text = self.title
-        authorLabel?.text = self.author
+        if article == nil {
+            return
+        }
+        
+        self.title = article!.title
+        
+        titleLabel?.text = article!.title
+        authorLabel?.text = article!.author
         
         webView?.navigationDelegate = self
+        webView?.scrollView.isScrollEnabled = false
         
-        let request = URLRequest(url: URL(string: "https://learnappmaking.com/lipsum.html")!)
-        webView?.load(request)
-
+        webView?.loadHTMLString("""
+            <html>
+                <head>
+                <style>body { font-family: -apple-system, Helvetica; sans-serif; }</style>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                </head>
+                <body>
+                    \(article!.content)
+                </body>
+            </html>
+            """, baseURL: nil)
+        
+        //webView?.loadHTMLString(article!.content, baseURL: nil)
+        
+//        titleLabel?.text = self.title
+//        authorLabel?.text = self.article?.author
+        
+//        let request = URLRequest(url: URL(string: "https://learnappmaking.com/lipsum.html")!)
+//        webView?.load(request)
+        
+        imageView?.image = nil
+        
+        if let thumbnailURL = URL(string:article!.thumbnailURL) {
+            Alamofire.request(thumbnailURL).responseData { response in
+                
+                if let data = response.result.value {
+                    self.imageView?.image = UIImage(data: data)
+                }
+            }
+        }
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
